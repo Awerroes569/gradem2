09fa511aa9f24738e77d5decf1d9dedf934d670e
Awerroes <arssoo569@gmail.com>
2024-04-03 21:35:29 +0200

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Hero from './components/Hero/Hero'
+import List from './components/List/List'
+import SearchForm from './components/SearchForm/SearchForm'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <SearchForm />
+      <List />  
+    </div>
+  );
+};
+
+export default App;
